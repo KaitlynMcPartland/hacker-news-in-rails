c56a7164e09082de9a14ee4949d82c782edb4aa0
Kaitlyn Adelaide & RajDesai <pair+kaitlynmcpartland.raj7desai@devbootcamp.com>
2016-08-01 18:07:28 -0700

    Add routes and controller methods for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: [:index, :new, :create, :destroy, :show]
+  resources :posts
+  resources :comments, only: [:index, :create, :new, :destroy]
+
 end
